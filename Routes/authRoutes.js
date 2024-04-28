4e5cf2934efb08b47078ef667d68bff32aa3d45b
GiuranRadu <117910448+GiuranRadu@users.noreply.github.com>
2024-04-28 21:12:22 +0300

    after removing unnecesary multer code

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,24 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../Controllers/authController');
-const multer = require('multer');
-const path = require('path');
-const picturePath = multer.diskStorage({
-  //note: where to store the uploaded pictures , the 3rd parameter is a random name choosed by me
-  destination: function (req, file, uploadPathHandler) {
-    uploadPathHandler(null, "uploads/")
-  },
-  //note: format uploaded file name (optional), the 3rd parameter is a random name choosed by me
-  filename: function (req, file, fileNameHandler) {
-    fileNameHandler(null, file.originalname) //! save it with the original name
-  }
-})
 
-const upload = multer({ storage: picturePath }).single('picture'); // Specify 'picture' as the field name
+
 
 //* ROUTES *
 router.route('/register')
-  .post(upload, authController.register)
+  .post(authController.register)
 
 router.route('/login')
   .post(authController.login)
--- Routes/usersRoutes.js
+++ Routes/usersRoutes.js
@@ -3,26 +3,11 @@ const usersController = require('./../Controllers/usersController');
 const router = express.Router();
 const authController = require('../Controllers/authController')
 
-const multer = require('multer');
-
-const picturePath = multer.diskStorage({
-  //note: where to store the uploaded pictures , the 3rd parameter is a random name choosed by me
-  destination: function (req, file, uploadPathHandler) {
-    uploadPathHandler(null, "uploads/")
-  },
-  //note: format uploaded file name (optional), the 3rd parameter is a random name choosed by me
-  filename: function (req, file, fileNameHandler) {
-    fileNameHandler(null, file.originalname) //! save it with the original name
-  }
-})
-
-const upload = multer({ storage: picturePath }).single('picture'); // Specify 'picture' as the field name
-
 
 //* PROTECT ALL BELOW ROUTES  *
 //note: pt ca linia este scrisa deasupra `ROUTES`, va trece mai intai prin aceasta, apoi prin celelalte
 //! GLOBAL
-router.use(authController.protect) 
+router.use(authController.protect)
 
 
 // * ROUTES *
@@ -33,7 +18,7 @@ router.route('/')
 
 router.route('/:id')
   .get(usersController.getUser)
-  .patch(upload, usersController.updateUser) //! Include the multer middleware here
+  .patch(usersController.updateUser) //! Include the multer middleware here
   .delete(usersController.deleteUser)
 
 
